943a02d8cb514114b440e51f76fe2d032afe15e7
AmadiMichael <amadimichaeld@gmail.com>
2023-02-05 20:13:20 +0100

    ctf tests for forwarder contract

---
+++ test/Forwarder.js
@@ -0,0 +1,44 @@
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const WALLET_NAME = "Wallet";
+const FORWARDER_NAME = "Forwarder";
+const NAME = "Forwarder tests"
+
+describe(NAME, function () {
+  async function setup() {
+    const [, attackerWallet] = await ethers.getSigners();
+      const value = ethers.utils.parseEther("1");
+
+      const forwarderFactory = await ethers.getContractFactory(FORWARDER_NAME);
+      const forwarderContract = await forwarderFactory.deploy();
+
+      const walletFactory = await ethers.getContractFactory(WALLET_NAME);
+      const walletContract = await walletFactory.deploy(forwarderContract.address, { value: value });
+
+      return { walletContract, forwarderContract, attackerWallet };
+  }
+
+  describe("exploit", async function () {
+      let walletContract, forwarderContract, attackerWallet, attackerWalletBalanceBefore;
+      before(async function () {
+          ({ walletContract, forwarderContract, attackerWallet } = await loadFixture(setup));
+          attackerWalletBalanceBefore = await ethers.provider.getBalance(attackerWallet.address);
+      })
+
+      it("conduct your attack here", async function () {
+        
+      });
+
+      after(async function () {
+          const attackerWalletBalanceAfter = await ethers.provider.getBalance(attackerWallet.address);
+          expect(attackerWalletBalanceAfter.sub(attackerWalletBalanceBefore)).to.be.equal(ethers.utils.parseEther("1"));
+
+          const walletContractBalance = await ethers.provider.getBalance(walletContract.address);
+          expect(walletContractBalance).to.be.equal('0');
+      });
+  });
+});

719470082905a983305fea73b61e449e301de418
Jesserc <raymondjesse713@gmail.com>
2023-02-13 09:12:28 +0100

    removed unused IAMM interface

--- contracts/FlashLoanCTF/Interface/IAMMOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-interface IAMMOracle {
-    function getLendTokenToEthPrice(uint) external returns (uint);
-
-    function getEthToLendTokenPrice(uint) external returns (uint);
-}
--- contracts/FlashLoanCTF/Lending.sol
+++ contracts/FlashLoanCTF/Lending.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./Interface/IAMMOracle.sol";
 import "./AMM.sol";
 
 contract Lending {

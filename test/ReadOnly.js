5d2e7408c50c5ce86b73b020a0e538849adcc310
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-04-03 20:05:31 +0200

    Remove unused transfer of eth to atackerWallet

--- test/ReadOnly.js
+++ test/ReadOnly.js
@@ -1,7 +1,4 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
@@ -9,64 +6,52 @@ const { ethers } = require("hardhat");
 const NAME = "ReadOnlyPool";
 
 describe(NAME, function () {
-  async function setup() {
-    const [, attackerWallet] = await ethers.getSigners();
+    async function setup() {
+        const [, attackerWallet] = await ethers.getSigners();
 
-    const ReadOnlyFactory = await ethers.getContractFactory(NAME);
-    const readOnlyContract = await ReadOnlyFactory.deploy();
+        const ReadOnlyFactory = await ethers.getContractFactory(NAME);
+        const readOnlyContract = await ReadOnlyFactory.deploy();
 
-    const VulnerableDeFiFactory = await ethers.getContractFactory(
-      "VulnerableDeFiContract"
-    );
-    const vulnerableDeFiContract = await VulnerableDeFiFactory.deploy(
-      readOnlyContract.address
-    );
+        const VulnerableDeFiFactory = await ethers.getContractFactory("VulnerableDeFiContract");
+        const vulnerableDeFiContract = await VulnerableDeFiFactory.deploy(readOnlyContract.address);
 
-    await readOnlyContract.addLiquidity({
-      value: ethers.utils.parseEther("100"),
-    });
-    await readOnlyContract.earnProfit({ value: ethers.utils.parseEther("1") });
-    await vulnerableDeFiContract.snapshotPrice();
+        await readOnlyContract.addLiquidity({
+            value: ethers.utils.parseEther("100"),
+        });
+        await readOnlyContract.earnProfit({ value: ethers.utils.parseEther("1") });
+        await vulnerableDeFiContract.snapshotPrice();
 
-    await network.provider.send("hardhat_setBalance", [
-      attackerWallet.address,
-      "0x1000",
-    ]);
+        // you start with 2 ETH
+        await network.provider.send("hardhat_setBalance", [
+            attackerWallet.address,
+            ethers.utils.parseEther("2.0").toHexString(),
+        ]);
 
-    // you start with 2 ETH
-    await network.provider.send("hardhat_setBalance", [
-      attackerWallet.address,
-      ethers.utils.parseEther("2.0").toHexString(),
-    ]);
+        return {
+            readOnlyContract,
+            vulnerableDeFiContract,
+            attackerWallet,
+        };
+    }
 
-    return {
-      readOnlyContract,
-      vulnerableDeFiContract,
-      attackerWallet,
-    };
-  }
+    describe("exploit", async function () {
+        let readOnlyContract, vulnerableDeFiContract, attackerWallet;
+        before(async function () {
+            ({ readOnlyContract, vulnerableDeFiContract, attackerWallet } = await loadFixture(setup));
+        });
 
-  describe("exploit", async function () {
-    let readOnlyContract, vulnerableDeFiContract, attackerWallet;
-    before(async function () {
-      ({ readOnlyContract, vulnerableDeFiContract, attackerWallet } =
-        await loadFixture(setup));
-    });
-
-    // prettier-ignore
-    it("conduct your attack here", async function () {
+        // prettier-ignore
+        it("conduct your attack here", async function () {
     
     });
 
-    after(async function () {
-      console.log(await vulnerableDeFiContract.lpTokenPrice());
-      expect(await vulnerableDeFiContract.lpTokenPrice()).to.be.equal(
-        0,
-        "snapshotPrice should be zero"
-      );
-      expect(
-        await ethers.provider.getTransactionCount(attackerWallet.address)
-      ).to.lessThan(3, "must exploit two transactions or less");
+        after(async function () {
+            console.log(await vulnerableDeFiContract.lpTokenPrice());
+            expect(await vulnerableDeFiContract.lpTokenPrice()).to.be.equal(0, "snapshotPrice should be zero");
+            expect(await ethers.provider.getTransactionCount(attackerWallet.address)).to.lessThan(
+                3,
+                "must exploit two transactions or less"
+            );
+        });
     });
-  });
 });
